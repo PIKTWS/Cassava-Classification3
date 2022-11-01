8f2ad0895cc11d2d90614aaf0339450f934a8404
Phongit Khanthawisood <87407078+PIKTWS@users.noreply.github.com>
2022-11-01 14:01:51 +0700

    Update app.py

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from keras.preprocessing.image import ImageDataGenerator
 import tensorflow as tf
 import numpy as np
 import streamlit as st
+import PIL
 
 # load model
 model = tf.keras.models.load_model("./EffNetB0_model.h5")
