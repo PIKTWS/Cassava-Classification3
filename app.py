51e11c0b3aa291eeccac1da9b56077d189e9ba2b
Phongit Khanthawisood <87407078+PIKTWS@users.noreply.github.com>
2022-11-21 23:36:30 +0700

    Update app.py

--- app.py
+++ app.py
@@ -71,7 +71,7 @@ if submit:
                 st.success('Good News! This leaf is healthy')
         else:
             st.error("Oh no! this leaf is "+result)
-            st.info("You Should ........")
+            #st.info("You Should ........")
 
         with tf.GradientTape() as tape:
             last_conv_layer = model.get_layer('top_conv')
